9e0b8267087de974199c0656b7c662a514b5ed24
qxchuckle <1934009145@qq.com>
2025-07-15 15:51:12 +0800

    feat: 支持多选删除

--- src/draftsContainer/commends/delete.ts
+++ src/draftsContainer/commends/delete.ts
@@ -1,27 +1,47 @@
-import * as vscode from "vscode";
-import { FileTreeDataProvider } from "../treeView/fileTreeDataProvider";
-import { FileItem } from "../treeView/fileItem";
-// import * as path from "path";
+import * as vscode from 'vscode';
+import { FileTreeDataProvider } from '../treeView/fileTreeDataProvider';
+import { FileItem } from '../treeView/fileItem';
+import * as path from 'path';
 
 export function createDelete(provider: ref<FileTreeDataProvider>) {
   return vscode.commands.registerCommand(
-    "qx-drafts.delete",
-    async (fileItem: FileItem) => {
-      if (!provider.value || !fileItem.resourceUri) {
-        return;
-      }
-      const confirm = await vscode.window.showWarningMessage(
-        `你确定要删除 ${fileItem.resourceUri.fsPath} 吗？`,
-        { modal: true },
-        "确定"
-      );
-      if (confirm === "确定") {
-        await vscode.workspace.fs.delete(fileItem.resourceUri, {
-          useTrash: true, // 使用回收站
-          recursive: true // 递归删除
-        });
-        provider.value.refresh();
-      }
-    }
+    'qx-drafts.delete',
+    async (fileItem: FileItem, fileItems?: FileItem[]) => {
+      const deleteFiles = fileItems ? fileItems : [fileItem];
+      await deleteMultipleFiles(deleteFiles);
+    },
   );
+
+  /**
+   * 删除多个文件
+   */
+  async function deleteMultipleFiles(fileItems: FileItem[]) {
+    if (!provider.value || !fileItems.length) {
+      return;
+    }
+    const len = fileItems.length;
+    const fileNames = fileItems
+      .map((item) => path.basename(item.resourceUri?.fsPath ?? ''))
+      .filter((item) => item !== '')
+      .join('\n');
+    const confirm = await vscode.window.showWarningMessage(
+      `确定要删除 ${len} 个文件吗？\n${fileNames}`,
+      { modal: true },
+      '确定',
+    );
+    if (confirm === '确定') {
+      for (const fileItem of fileItems) {
+        if (!fileItem.resourceUri) {
+          continue;
+        }
+        try {
+          await vscode.workspace.fs.delete(fileItem.resourceUri, {
+            useTrash: true, // 使用回收站
+            recursive: true, // 递归删除
+          });
+        } catch (error) {}
+      }
+      provider.value.refresh();
+    }
+  }
 }
